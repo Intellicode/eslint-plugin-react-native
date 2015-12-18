a29af2bf0688ab92b457248378f51d3c056634c9
Tom Hastjarjanto <tom@intellicode.nl>
2015-12-18 22:42:20 +0100

    Add to plugin

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 module.exports = {
   rules: {
-    'no-unused-styles': require('./lib/rules/no-unused-styles')
+    'no-unused-styles': require('./lib/rules/no-unused-styles'),
+    'split-platform-components': require('./lib/rules/split-platform-components')
   },
   rulesConfig: {
-    'no-unused-styles': 0
+    'no-unused-styles': 0,
+    'split-platform-components': 0
   }
 };
